2df56b9781ff2d1f51862b417d4e820fe8eb0a22
helloservices00-blip <helloservices00@gmail.com>
2025-11-23 11:38:07 +0530

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,16 +2,18 @@ import { Link } from "react-router-dom";
 
 export default function Profile() {
   return (
-    <div>
-      <h2>Please Login</h2>
-      <Link to="/login">
-        <button>Login</button>
-      </Link>
+    <div style={{ padding: "20px" }}>
+      <h2>User Profile</h2>
 
-      <p>Don't have an account?</p>
-      <Link to="/signup">
-        <button>Create Account</button>
-      </Link>
+      <div style={{ marginTop: "20px" }}>
+        <Link to="/login">
+          <button style={{ marginRight: "10px" }}>Login</button>
+        </Link>
+
+        <Link to="/signup">
+          <button>Signup</button>
+        </Link>
+      </div>
     </div>
   );
 }
