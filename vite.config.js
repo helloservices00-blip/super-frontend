8ca55d2faefc347fa8d34658f78490c22225486a
helloservices00-blip <helloservices00@gmail.com>
2025-11-23 11:33:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // required for Render
+    outDir: "dist",
   },
   server: {
-    port: 5173, // optional for local dev
-  },
-  define: {
-    'process.env': process.env, // allows VITE_API_URL usage
+    historyApiFallback: true,
   },
 });
